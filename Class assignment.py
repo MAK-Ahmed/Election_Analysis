d4fce8a1035077c1d0b87c6b90564920c9b4dea9
MAK-Ahmed <MAhmed27@northwell.edu>
2022-09-07 21:35:53 -0400

    adding the election results

---
+++ Class assignment.py
@@ -0,0 +1,22 @@
+# Create a Python list to store your grocery list
+    
+Grocery_list = ["Milk", "Bread","Eggs", "Peanut Butter","Jelly"]
+
+# Print the grocery list
+print(Grocery_list)
+
+# Change "Peanut Butter" to "Almond Butter" and print out the updated list
+Grocery_list[3] = "Almond Butter"
+
+print(Grocery_list)
+
+# Remove "Jelly" from grocery list and print out the updated list
+
+Grocery_list.remove("Jelly")
+print(Grocery_list)
+
+# Add "Coffee" to grocery list and print the updated list
+
+Grocery_list.append("Coffee")
+print(Grocery_list)
+
